9103d8570e1a5a24172f20bd88fa945bb3739e66
Houria <126777373+Hou-ry@users.noreply.github.com>
2025-06-30 15:41:13 +0100

    added the search input + style

--- src/components/imageGenerator/imageGen.jsx
+++ src/components/imageGenerator/imageGen.jsx
@@ -7,10 +7,14 @@ export const ImageGen = () => {
   return (
     <div className='ai_image_generator'>
       <div className="header">
-      Ai image <span>generator </span>
+        Ai Image <span>Generator </span>
       </div>
-      <div className="image">
-        <img src={default_img} alt="" />
+      <div className="image-loading">
+        <div className="image"><img src={default_img} alt="" /></div>
+      </div>
+      <div className="search-box">
+        <input type="text" className='search-input' placeholder='Describe what you want to see' />
+        <button className='Generate-btn'>Generate</button>
       </div>
     </div>
   )
